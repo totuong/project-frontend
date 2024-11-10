118cea4fedbacb707190d361703ce11ec2e6e2af
Tuongtd <tuongtd2@adflex.vn>
2024-11-10 23:43:50 +0700

    23

--- src/views/user/component/Profile/Bio.vue
+++ src/views/user/component/Profile/Bio.vue
@@ -19,50 +19,56 @@
         />
       </el-tooltip>
     </div>
-    <div class="flex flex-row">
-        <div
-          class="avatar-container ml-6 flex items-center absolute"
+    <el-row :gutter="24" class="relative flex flex-row h-[100px] justify-between">
+     
+      <div class="avatar-container ml-12 flex items-center absolute bottom-0">
+        <img
+          src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+          alt="Avatar"
+          class="avatar-image w-30 h-30 rounded-full border-4 border-white"
+        />
+        <el-tooltip
+          class="box-item"
+          effect="dark"
+          content="Chỉnh sửa ảnh đại diện"
+          placement="top"
         >
-          <img
-            src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-            alt="Avatar"
-            class="avatar-image w-30  h-30 rounded-full border-4 border-white"
+          <el-button
+            :icon="Edit"
+            class="edit-cover absolute bottom-[25px] right-[10px]"
+            circle
           />
-          <el-tooltip
-            class="box-item"
-            effect="dark"
-            content="Chỉnh sửa ảnh đại diện"
-            placement="top"
-          >
-            <el-button
-              :icon="Edit"
-              class="edit-cover absolute bottom-[25px] right-[10px]"
-              circle
-            />
-          </el-tooltip>
-        </div>
-        <div class="info-avatar">
-          <h2 class="username text-2xl font-semibold">Tô Tường</h2>
-          <h2 class="follower text-xl font-medium opacity-50 ">3 followers</h2>
-        </div>
-        <div class="flex flex-row ">
-          <el-button type="primary" :icon="Edit">Chỉnh sửa thông tin cá nhân</el-button>
-          <el-button type="primary" :icon="Edit">Chỉnh sửa thông tin cá nhân</el-button>
-        </div>
+        </el-tooltip>
+      </div>
+      <el-col :span="4" class="info-avatar ml-60 mt-4">
+        <h2 class="username text-2xl font-semibold">Tô Tường</h2>
+        <h2 class="follower text-xl font-medium opacity-50">3 followers</h2>
+      </el-col>
+      <el-col :span="10" class="flex justify-end content-center">
+    <div class="w-full flex justify-end space-x-2">
+      <el-button type="primary" icon="Edit">Chỉnh sửa thông tin cá nhân</el-button>
+      
+      <el-dropdown>
+        <el-button type="primary">
+        
+          <el-icon class="el-icon--right"><arrow-down /></el-icon>
+        </el-button>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>Video</el-dropdown-item>
+            <el-dropdown-item>Ảnh</el-dropdown-item>
+            <el-dropdown-item>Action 3</el-dropdown-item>
+            <el-dropdown-item>Chọc</el-dropdown-item>
+            <el-dropdown-item>Báo cáo</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
     </div>
+  </el-col>
 
-    <div class="user-info mt-16 text-center">
-      <div class="flex gap-2 justify-center">
-        <el-tag type="primary">Ca hát</el-tag>
-        <el-tag type="success">Ảo thuật</el-tag>
-        <el-tag type="info">Nhảy múa</el-tag>
-      </div>
-      <p class="bio text-gray-600">Bằng</p>
-      <div class="actions mt-4 space-x-2">
-        <el-button type="primary" size="small">Add Friend</el-button>
-        <el-button size="small">Message</el-button>
-      </div>
-    </div>
+    </el-row>
+    <hr></hr>
+
   </div>
 </template>
 
@@ -71,8 +77,10 @@ import { Edit } from "@element-plus/icons-vue";
 </script>
 
 <style scoped lang="scss">
-.el-col-4{
+.el-col-4 {
   right: 0;
 }
-
+.last-component{
+  right:0px;
+}
 </style>
--- src/views/user/component/Profile/profile.vue
+++ src/views/user/component/Profile/profile.vue
@@ -1,85 +1,20 @@
 <template>
   <div class="profile-page container mx-auto bg-white p-4 rounded-lg shadow-lg">
-    <Bio />
-    <el-tabs v-model="activeTab" @tab-click="handleTabClick" class="mt-6">
-      <el-tab-pane label="Posts" name="posts">
-        <Posts />
-      </el-tab-pane>
-
-      <el-tab-pane label="Friends" name="friends">
-        <Friend />
-      </el-tab-pane>
-
-      <el-tab-pane label="About" name="about">
-        <About />
-      </el-tab-pane>
-    </el-tabs>
+    <Bio class="mb-5" />
+    <el-row :gutter="24" class="flex-wrap">
+      <el-col :span="8"> <BookInfo /></el-col>
+      <el-col :span="16">
+        <el-container>
+          <Tab /> </el-container
+      ></el-col>
+    </el-row>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
 import Bio from "./Bio.vue";
-import Friend from "./Friend.vue";
-import About from "./About.vue";
-import Posts from "./Posts.vue";
-
-const activeTab = ref("posts");
-
-const handleTabClick = (tab: any) => {
-  console.log("Selected tab:", tab.label);
-};
+import BookInfo from "./BookInfo.vue";
+import Tab from "./Tab.vue";
 </script>
 
-<style lang="scss" scoped>
-.profile-page {
-  max-width: 80%;
-  width: 100%;
-
-  .cover-photo {
-    position: relative;
-
-    .cover-image {
-      object-fit: cover;
-    }
-
-    .avatar {
-      .avatar-image {
-        border: 4px solid white;
-      }
-    }
-  }
-
-  .user-info {
-    margin-top: 60px;
-
-    .username {
-      font-size: 24px;
-      font-weight: bold;
-    }
-    .bio {
-      color: gray;
-    }
-  }
-
-  .post {
-    margin-top: 20px;
-  }
-
-  .post-header {
-    .post-avatar {
-      margin-right: 10px;
-    }
-  }
-
-  .friend-avatar {
-    border-radius: 8px;
-  }
-
-  .about-section {
-    p {
-      margin: 5px 0;
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
