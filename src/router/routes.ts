cc8c9ae26834286f3658c50c9d83bfb36b836ca0
To Duy Tuong <tuongtd2@adflex.vn>
2025-06-03 10:44:44 +0700

    delete acl

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -149,53 +149,6 @@ export const constantRoute = [
   },
 ];
 
-export const asyncRoute = [
-  {
-    path: "/acl",
-    component: () => import("@/layout/index.vue"),
-    name: "Acl",
-    meta: {
-      title: "权限管理",
-      hidden: false,
-      icon: "Lock",
-    },
-    redirect: "/acl/user",
-    children: [
-      {
-        path: "/acl/user",
-        component: () => import("@/views/acl/user/index.vue"),
-        name: "User",
-        meta: {
-          title: "用户管理",
-          hidden: false,
-          icon: "User",
-        },
-      },
-      {
-        path: "/acl/role",
-        component: () => import("@/views/acl/role/index.vue"),
-        name: "Role",
-        meta: {
-          title: "角色管理",
-          hidden: false,
-          icon: "Avatar",
-        },
-      },
-      {
-        path: "/acl/permission",
-        component: () => import("@/views/acl/permission/index.vue"),
-        name: "Permission",
-        meta: {
-          title: "菜单管理",
-          hidden: false,
-          icon: "List",
-        },
-      },
-    ],
-  },
- 
-];
-
 export const anyRoute = {
   path: "/:pathMatch(.*)*",
   redirect: "/404",
